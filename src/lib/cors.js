24fca4ef37044c3bef4fda69a500065181f3e578
yuphyealorn-ops <yuphyea.lorn@gmail.com>
2026-01-23 11:08:32 +0700

    update 1

--- src/lib/cors.js
+++
@@ -1,8 +0,0 @@
-let corsHeaders = { 
-    "Access-Control-Allow-Origin": "*", 
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS", 
-    "Access-Control-Allow-Headers": "Content-Type, Authorization", 
-    "Access-Control-Max-Age": "86400", 
-}; 
-
-export default corsHeaders; 
