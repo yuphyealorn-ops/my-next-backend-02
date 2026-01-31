9dfafcac9b0292a30db1c448b39792896ec4e09a
yuphyealorn-ops <yuphyea.lorn@gmail.com>
2026-01-31 23:33:58 +0700

    item crud testing

--- src/lib/cors.js
+++ src/lib/cors.js
@@ -1,6 +1,6 @@
 let corsHeaders = { 
     "Access-Control-Allow-Origin": "*", 
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS", 
+    "Access-Control-Allow-Methods": "GET,POST,PATCH,PUT,OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization", 
     "Access-Control-Max-Age": "86400", 
 }; 
